860a2a96baa48fa6303fbbac8fac67b50bcbc051
laszlo-balogh <balaszlo444@gmail.com>
2026-09-03 15:35:49 +0200

    initial implementation of entities

---
+++ backend/Domain/FoodItem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain
+{
+    public class FoodItem
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public required string Name { get; set; }
+        public int Quantity { get; set; }
+        public required string Unit { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public required string Category { get; set; }
+        public required string GroupId { get; set; }
+
+        [ForeignKey("GroupId")]
+        public virtual required FridgeGroup Group { get; set; }
+
+        public required string AddedByUserId { get; set; }
+
+        [ForeignKey("AddedByUserId")]
+        public virtual required AppUser AddedByUser { get; set; }
+    }
+}
---
+++ backend/Domain/FridgeGroup.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class FridgeGroup
+    {
+        [Key] 
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public required string GroupName { get; set; }
+
+        public required string AdminUserId { get; set; }
+
+        public virtual ICollection<AppUser> Members { get; set; } = new List<AppUser>();
+
+        public virtual ICollection<FoodItem> FoodItems { get; set; } = new List<FoodItem>();
+
+        public virtual ICollection<ShoppingListItem> ShoppingListItems { get; set; } = new List<ShoppingListItem>();
+    }
+}
