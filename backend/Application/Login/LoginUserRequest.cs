a8b05fbc2843a84de2da8b371a55101da2bd8b60
laszlo-balogh <balaszlo444@gmail.com>
2026-09-23 23:55:11 +0200

    create login: dto, validator and service

---
+++ backend/Application/Login/LoginUserRequest.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Application.Login
+{
+    public class LoginUserRequest
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+
+    }
+}
---
+++ backend/Application/Login/LoginUserValidator.cs
@@ -0,0 +1,19 @@
+﻿using Application.Registration;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Login
+{
+    public class LoginUserValidator : AbstractValidator<LoginUserRequest>
+    {
+        public LoginUserValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.");
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required.");
+        }
+    }
+}
