243d65ec2435e4be6e6c480d42b0f7a530c3722a
laszlo-balogh <balaszlo444@gmail.com>
2026-09-24 00:36:17 +0200

    configure jwt bearer

--- backend/Api/Program.cs
+++ backend/Api/Program.cs
@@ -4,13 +4,18 @@ using Application.Registration;
 using FluentValidation;
 using Infrastructure.Identity;
 using Infrastructure.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+var jwtSetting = builder.Configuration.GetSection("Jwt").Get<JwtSettings>() 
+    ?? throw new InvalidOperationException("Jwt settings are missing from configuration.");
 
 // Add services to the container.
 
@@ -31,12 +36,31 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true, ValidIssuer = jwtSetting.Issuer,
+        ValidateAudience = true, ValidAudience = jwtSetting.Audience,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,                
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSetting.Key))
+    };
+});
 
 builder.Services.AddScoped<IAppDbContext>(provider => provider.GetRequiredService<AppDbContext>());
+
 builder.Services.AddScoped<IIdentityService, IdentityService>();
 builder.Services.AddScoped<ITokenService, JwtTokenService>();
+
 builder.Services.AddScoped<RegisterUserService>();
 builder.Services.AddScoped<LoginUserService>();
+
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 {
     options.User.RequireUniqueEmail = true;
